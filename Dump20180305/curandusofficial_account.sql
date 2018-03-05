72f263a609f774c443d9c622191730d729c4386b
ivanarancicrancic <34153188+ivanarancicrancic@users.noreply.github.com>
2018-03-05 15:56:30 +0100

    Add files via upload

---
+++ Dump20180305/curandusofficial_account.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE  IF NOT EXISTS `curandusofficial` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `curandusofficial`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 104.245.33.167    Database: curandusofficial
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `account`
+--
+
+LOCK TABLES `account` WRITE;
+/*!40000 ALTER TABLE `account` DISABLE KEYS */;
+INSERT INTO `account` VALUES (3,'\0','','\0','\0','papidakos','cpapidas'),(5,'\0','','\0','\0','b8f57d6d6ec0a60dfe2e20182d4615b12e321cad9e2979e0b9f81e0d6eda78ad9b6dcfe53e4e22d1','cpapidas2'),(10,'\0','','\0','\0','$2a$10$h6cO.XkIuJ8I1kS2ZuaW/uRbv67.0ib7xOLdyLhAdWtN5cXRXSatW','cdddddc'),(11,'\0','','\0','\0','$2a$10$gTqi4RDXj5ovC.MoFHEoEOMMhPqjcvF/R49EjxwfUme6PwXhUHMwa','cc'),(13,'\0','','\0','\0','$2a$10$nlGCcuWdRcLDVOFYzudn5.tl4bhH47mkZDB3JubzbZy6obZVxgdmy','cd'),(15,'\0','','\0','\0','$2a$10$jT/N9/TgXnbGpdH0R1lfL.VlCihlR9jY7CBESimbtyAhOAwo9bguu','cs'),(16,'\0','','\0','\0','$2a$10$wupaAG1Cu8rGI4.kapj7rebxjGKfwjBYjkWbq.F.ZQKyiegu0exrq','wesdfsdfsdfs'),(17,'\0','','\0','\0','$2a$10$QufI1ku2vBT1NBCENVAKUeHsaktKTGZPzn/I66WdcTnXqEhbqAaC6','yy'),(18,'\0','','\0','\0','$2a$10$1OOIk5weuiDjS8S9BKpP3uBohiQ/RaLJjUJfiqdn8IIumXQ6hXn3K','mm33'),(19,'\0','','\0','\0','$2a$10$AZJQX.Zppg2.5geTAnC6V.XUJt.NGtYROpxoKpOdBCHoDCtbU/cNa','mm33dfgdfg'),(20,'\0','','\0','\0','$2a$10$lWh.9oyG1fz7NaMrEd32m.hLm4ucEhQdanNO09fmVFZB1YxyRC3ua','wesdfsdllfsdfs'),(21,'\0','','\0','\0','$2a$10$3ybRtrgCf1D.01aeWwheluIaOEJ8FRw0UZ7LBG.yzvY1.2/QTCghK','asdfafsd234'),(22,'\0','','\0','\0','$2a$10$x1loX5n0.b5MHDSfLWEJduhZgNU.6aVlaxAJ57PlWs.B.FlFDhL3K','asdfafsd234e'),(23,'\0','','\0','\0','$2a$10$qSyGWdtTl2w2ubms8P2c1.PR4.QAxFuAaFMyA133XsGXJfPNXnVzi','sdfsdf333'),(24,'\0','','\0','\0','$2a$10$/Dtd5WqgGTmTGDaB8Sd6vOTdyCu4qc9G94Igukv3QONpuB.aUkBue','sdfsdf333s'),(25,'\0','','\0','\0','$2a$10$MA50o0uzuyqERecKD7Rl4uqdlNDAm1x/4nE2.1cJ1DTvsf8799i.O','fasdf3333'),(26,'\0','','\0','\0','$2a$10$KL00afcwiDbq24EBev1Lj.CG5WSgydNra3LlmRu3CWJMIlNF/TQcO','fasdf33332'),(27,'\0','','\0','\0','$2a$10$PY9L787.w5VgoO9ZH0S62OQOz31.yARV6w.F2QfiRxbPTktZ0s9oy','Christost22'),(28,'\0','','\0','\0','$2a$10$Q9XKh8cc4J7qy0cy9OSksuXhOwMgoghPof8ILZxsULuk3gsMr65gC','asdfasdf234'),(29,'\0','','\0','\0','$2a$10$q4FAN8hsZKwEGWWmKUJK4uan4hBHhq2nd.NLm.tc2qDq3COjHfkz6','adsfsda234'),(30,'\0','','\0','\0','$2a$10$oXvocLdaQVC3n4wKYEtON./1EnOn8Jk5E9XTQEYHvJEjD7uj12Pvy','asdfasdf1234'),(31,'\0','','\0','\0','$2a$10$OFLSsg0x5A36HPq3.2wEDexxPlHQzWvQGo5rmfG5P6eeORtaQ/Ubq','asfdasdf3433'),(32,'\0','','\0','\0','$2a$10$XlUPeD83cMNbnjnXnXHcxubm0n9SrBd.LsCB3wxwAge6tYhwhumZq','fasfafd324'),(38,'\0','','\0','\0','$2a$10$SdZfafMVkFopgdvi/JrBn.h1KDIt.3Dfx6lbhLWB/CiGIBNiGGg/i','asdfasdf2344'),(39,'\0','','\0','\0','$2a$10$7sibRze3x6SVstcQYXSDe.LqhE0gUHvSfDTVZKyURr9EzKykw4sry','dsfsdff444'),(40,'\0','','\0','\0','$2a$10$EglFcHZnRyzrlmvb1ldV9e89IL1IqgdLhgeG/WGAR9UeXRP0fNYnW','ddsdsdfffd'),(41,'\0','','\0','\0','$2a$10$5RcLNvOD.r03ZayV53MxVuUQYbJvDtMrRFTTUVL9G2X0S6/ZxEo3S','papidakos'),(42,'\0','','\0','\0','$2a$10$eR.90pak84ipqOq9HVEMq.VgQ8O9JOFGOYROCzNzb7hdRhI/cEmem','papidakos2'),(43,'\0','','\0','\0','spring','roy'),(44,'\0','','\0','\0','$2a$10$FbaEGdHDEEvpPHjSZkWNyecmsL79lH8qzJKexRo5EH4shvZfd4WhG','papapakiki'),(45,'\0','','\0','\0','$2a$10$69qe1gDLxu9T2n6fIV6s3.bY.uvNBEG/GvLcw7enccZ4HFLY2sMHy','papapakiki1'),(46,'\0','','\0','\0','$2a$10$qBQBPCNXVGd5l.qZd47MKuI.M88K8WiwbigPUxpUbx1WJA5b8oK8q','papalolif'),(47,'\0','','\0','\0','$2a$10$6LZfgsqeunoY4gLc1EZbe..6q5seZrA191F6iH81EeI709Y0h.1Py','fafafakoko');
+/*!40000 ALTER TABLE `account` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-05 15:25:33
---
+++ Dump20180305/curandusofficial_treatmentitem.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE  IF NOT EXISTS `curandusofficial` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `curandusofficial`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 104.245.33.167    Database: curandusofficial
+-- ------------------------------------------------------
+-- Server version	5.7.19-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `treatmentitem`
+--
+
+LOCK TABLES `treatmentitem` WRITE;
+/*!40000 ALTER TABLE `treatmentitem` DISABLE KEYS */;
+INSERT INTO `treatmentitem` VALUES (3437,1128,'1','ACK','4','1','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3438,1128,'2','ACK','4','4','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3441,1130,'1','ACK','','4','\"{}\"','0',NULL,NULL,NULL,NULL,'true','Today','T.I.D.'),(3442,1130,'2','ACK','8','2','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3445,1130,'3','ACK','','5','\"{}\"','1',NULL,NULL,NULL,NULL,'true','Today','Q.I.D.'),(3447,1128,'1','ACK','4','4','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3448,1133,'3','ACK','4','4','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3449,1128,'3','ACK','4','4','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3450,1128,'1','ACK','4','4','\"{}\"','0',NULL,NULL,NULL,NULL,'false','Today',''),(3451,1134,'5','ACK','5','5','\"{\\\"medicinename\\\":\\\"ggg\\\"}\"','1',NULL,NULL,NULL,NULL,'false','Today',''),(3465,1141,'8','ACK','','2','\"{\\\"diet\\\":\\\"kh\\\"}\"','1',NULL,NULL,NULL,NULL,'false','In 5 days',''),(3466,1141,'1','ACK','2','2','\"{}\"','1',NULL,NULL,NULL,NULL,'false','Today',''),(3468,1143,'1','ACK','','2','\"{}\"','1',NULL,NULL,NULL,NULL,'true','Today','Every 2 days'),(3469,1144,'1','ACK','','3','\"{}\"','1',NULL,NULL,NULL,NULL,'true','Today','Q.I.D.'),(3471,1133,'1','ACK','4','4','\"{}\"','1',NULL,NULL,NULL,NULL,'false','Today',''),(3472,1128,'2','ACK','4','4','\"{}\"','1',NULL,NULL,NULL,NULL,'false','Today','');
+/*!40000 ALTER TABLE `treatmentitem` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-05 15:25:11
